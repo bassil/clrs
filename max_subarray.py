f0ea73310ae78a3488bb72218a2eecd06ddc6bc4
Bassil <bassil.tabidi@gmail.com>
2020-12-03 16:16:20 -0500

    scaffold for max subarray problem

---
+++ max_subarray.py
@@ -0,0 +1,12 @@
+def max_subarray(A):
+	"""return the maximum subarray of A, the nonempty contiguous 
+	subarray of A whose values have the largest sum
+
+	"""
+	return A
+
+
+if __name__ == '__main__':
+	
+	A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+	print(max_subarray(A))
