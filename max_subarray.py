9d63e52a963634874eb0003f06fef0ca12a04966
Bassil <bassil.tabidi@gmail.com>
2020-12-03 16:23:26 -0500

    Implemented brute force approach to max subarray problem

--- max_subarray.py
+++ max_subarray.py
@@ -1,9 +1,20 @@
 def max_subarray(A):
 	"""return the maximum subarray of A, the nonempty contiguous 
 	subarray of A whose values have the largest sum
-
 	"""
-	return A
+	# brute force: try every pair of dates 
+	# where the sell date is after the buy date
+	max_sum = 0
+	best_buy_date = 0
+	best_sell_date = 0
+	for buy_date in range(len(A)):
+		for sell_date in range(buy_date, len(A)):
+			if sum(A[buy_date:sell_date]) > max_sum:
+				max_sum = sum(A[buy_date:sell_date])
+				best_buy_date = buy_date
+				best_sell_date = sell_date
+
+	return A[best_buy_date: best_sell_date], max_sum
 
 
 if __name__ == '__main__':
