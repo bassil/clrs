86844269e17734379c03aacf44fb1a868ac6c516
Bassil Tabidi <bassil.tabidi@gmail.com>
2020-12-03 17:37:19 -0500

    starting divide and conquer approach

--- max_subarray.py
+++ max_subarray.py
@@ -1,9 +1,12 @@
+import sys
+
 def max_subarray(A):
 	"""return the maximum subarray of A, the nonempty contiguous 
 	subarray of A whose values have the largest sum
+	brute force: try every pair of dates 
+	where the sell date is after the buy date
 	"""
-	# brute force: try every pair of dates 
-	# where the sell date is after the buy date
+	
 	max_sum = 0
 	best_buy_date = 0
 	best_sell_date = 0
@@ -16,8 +19,25 @@ def max_subarray(A):
 
 	return A[best_buy_date: best_sell_date], max_sum
 
+def max_crossing_subarray(A, low, mid, high):
+	"""return indices of a maximum subarray that crosses the middle of A, e.g., 
+	A[i..mid], A[mid+1..j], where low <= i <= mid <= j <= high"""
+	left_sum = -(sys.maxsize)
+	crossing_sum = 0
+
+	for left_i in range(mid, low, -1):
+		todo
+
+
+	return
 
 if __name__ == '__main__':
 	
 	A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
-	print(max_subarray(A))
+	
+	low = 0
+	mid = len(A)//2
+	high = len(A)
+
+	max_crossing_subarray(A, low, mid, high)
+
