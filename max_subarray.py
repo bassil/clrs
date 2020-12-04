ec9c60d1475ceed78885d5b35518887f5e5a5a55
Bassil <bassil.tabidi@gmail.com>
2020-12-04 16:02:12 -0500

    implemented max_crossing_subarray

--- max_subarray.py
+++ max_subarray.py
@@ -22,22 +22,38 @@ def max_subarray(A):
 def max_crossing_subarray(A, low, mid, high):
 	"""return indices of a maximum subarray that crosses the middle of A, e.g., 
 	A[i..mid], A[mid+1..j], where low <= i <= mid <= j <= high"""
+
 	left_sum = -(sys.maxsize)
 	crossing_sum = 0
+	min_left = 0
 
-	for left_i in range(mid, low, -1):
-		todo
+	for left_i in range(mid, low - 1, -1):
+		crossing_sum += A[left_i]
 
+		if crossing_sum > left_sum:
+			left_sum = crossing_sum
+			min_left = left_i
 
-	return
+	right_sum = -(sys.maxsize)
+	crossing_sum = 0
+	max_right = 0
+
+	for right_j in range(mid + 1, high):
+		crossing_sum += A[right_j]
+
+		if crossing_sum > right_sum:
+			right_sum = crossing_sum
+			max_right = right_j
+
+	return min_left, max_right, left_sum + right_sum
 
 if __name__ == '__main__':
 	
 	A = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
 	
 	low = 0
-	mid = len(A)//2
+	mid = len(A) // 2
 	high = len(A)
 
-	max_crossing_subarray(A, low, mid, high)
+	print(max_crossing_subarray(A, low, mid, high))
 
